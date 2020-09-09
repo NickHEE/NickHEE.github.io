f39c20a568812a8ac70706b10319c54204ba2e61
Nick <nick.huttemann@gmail.com>
2020-09-08 17:59:48 -0700

    Updates

---
+++ precache-manifest.a435fc1344866c8d7563c662608d725d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22f38d75d2ace106826130412e0a7c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57590056016a0a1fa28a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92155ba12620cb46c216",
+    "url": "/static/js/2.75d23d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75d23d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57590056016a0a1fa28a",
+    "url": "/static/js/main.df86e38a.chunk.js"
+  },
+  {
+    "revision": "68ee1f524f49a711ddc5",
+    "url": "/static/js/runtime-main.936283e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
